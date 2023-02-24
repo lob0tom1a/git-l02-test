315ff80454e1f21bab22e7ead249d38724554214
lob0tom1a <nikitakulichkin2003@gmail.com>
2023-02-24 12:44:21 +0300

    added function

--- src/master.java
+++ src/master.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class master {
     public static void main(String[] args) {
         System.out.println("Hello,"+args[0]);
+        double x = new Scanner(System.in).nextDouble();
+        double y = new Scanner(System.in).nextDouble();
+        double res = (3*Math.pow(Math.cos(x-Math.PI/6),2))/((1/2)+Math.sin(y)*Math.sin(y));
+        System.out.println(res);
     }
 }
